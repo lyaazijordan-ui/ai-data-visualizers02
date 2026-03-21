b40d49d4bd3bb25c1f95480652296c6c0eb2ae7c
lyaazijordan-ui <lyaazijordan@gmail.com>
2026-03-21 21:50:45 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,30 +1,23 @@
-# auth.py
 import streamlit as st
-
-# Demo persistent memory for users
-if "users" not in st.session_state:
-    st.session_state["users"] = {}
+from db import save_user_remote, load_user_remote
 
 def login(username, password):
-    users = st.session_state["users"]
-    if username in users and users[username]["password"] == password:
-        st.session_state["username"] = username
+    data = load_user_remote(username, "account") or {}
+    if data.get("password")==password:
+        st.session_state["username"]=username
+        st.session_state.update(load_user_remote(username, "settings") or {})
         return True
     return False
 
 def signup(username, password):
-    users = st.session_state["users"]
-    if username not in users:
-        users[username] = {"password": password, "theme":"plotly_dark", "chart_color":"Agsunset"}
+    if load_user_remote(username, "account") is None:
+        save_user_remote(username, "account", {"password":password})
+        save_user_remote(username, "settings", {"theme":"plotly_dark","chart_color":"Agsunset"})
         return True
     return False
 
 def load_user_settings(username):
-    users = st.session_state["users"]
-    return users.get(username, {})
+    return load_user_remote(username, "settings") or {}
 
 def save_user_settings(username, theme, chart_color):
-    users = st.session_state["users"]
-    if username in users:
-        users[username]["theme"] = theme
-        users[username]["chart_color"] = chart_color
+    save_user_remote(username, "settings", {"theme":theme,"chart_color":chart_color})
