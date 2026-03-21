aed9bf3962aeb7e0f29317cf813fdcee98e071bb
lyaazijordan-ui <lyaazijordan@gmail.com>
2026-03-21 21:41:29 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,25 +1,30 @@
 # auth.py
 import streamlit as st
 
-# In-memory user storage (demo)
-users = {}
+# Demo persistent memory for users
+if "users" not in st.session_state:
+    st.session_state["users"] = {}
 
 def login(username, password):
+    users = st.session_state["users"]
     if username in users and users[username]["password"] == password:
         st.session_state["username"] = username
         return True
     return False
 
 def signup(username, password):
+    users = st.session_state["users"]
     if username not in users:
         users[username] = {"password": password, "theme":"plotly_dark", "chart_color":"Agsunset"}
         return True
     return False
 
 def load_user_settings(username):
+    users = st.session_state["users"]
     return users.get(username, {})
 
 def save_user_settings(username, theme, chart_color):
+    users = st.session_state["users"]
     if username in users:
         users[username]["theme"] = theme
         users[username]["chart_color"] = chart_color
